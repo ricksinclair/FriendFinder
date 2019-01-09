47ada72e5c71bebe4a9421de0aa32529bcb67096
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-01-09 10:23:42 -0500

    html routes built

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,14 @@
-//API Routes//
-app.get("/api/friends", function(req, res) {
-  res.send("friends");
-});
+//Dependencies//
+var express = require("express");
+var path = require("path");
+
+module.exports = function(app) {
+  //API Routes//
+  app.get("/api/friends", function(req, res) {
+    res.send("friends");
+  });
+
+  //Initialize data parsing features//
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+};
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Dependencies//
 var express = require("express");
-var path = require("path");
 
 //Initialize express//
 var app = express();
 var PORT = process.env.port || 3000;
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-//Initialize data parsing features//
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.listen(PORT, function() {
+  console.log(`The Friend Finder App is now listening on PORT ${PORT}`);
+});
