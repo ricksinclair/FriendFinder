91b964a28905142c7e7ed5c2bf9b6a9a577a2352
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-03-04 15:24:49 -0500

    added to server.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,25 @@
 //Dependencies//
 var express = require("express");
-var path = require("path");
+
+//require mysql node module https://www.npmjs.com/package/mysql
+var mysql = require("mysql");
+
+//set Up connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
 
 module.exports = function(app) {
   //API Routes//
   app.get("/api/friends", function(req, res) {
     res.send("friends");
+    //Get data from database
+
+    //map data
+    //store it in object
+    //send object as response to the user
   });
 
   //Initialize data parsing features//
--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 //Dependencies//
-var express = require("express");
+let express = require("express");
+let bodyParser = require("body-parser");
 
 //Initialize express//
-var app = express();
-var PORT = process.env.port || 3000;
+let app = express();
+let PORT = process.env.port || 3000;
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
+//Body Parser references
+let jsonParser = bodyParser.json();
+let urlEncoded = bodyparser.urlecoded({ extended: false });
+let rawParser = bodyParser.raw();
+let textParser = bodyParser.text();
+//I had some questions on the need for body-parser if node already can send/receive data without using it
+//apparently it used to be part of the core express package but it was seprarated to give devs more control.
+//it can unzip and parse all sorts of data without needing to manipulate every little field.
+//it has 4 methods,
+//.raw() just exposes buffered content on req.body
+//.text()  reads buffer as plain text and exposes on req.body
+//.urlencoded() parses text as url encoded data (like how a browser sends form data)
+//.json parses text as json and exposes it to req. body
+
+//set up body parser for json
+app.use(jsonParser({ type: "application/**json" }));
+
+//set up bodyparser for raw content
+
+//the .vnd spec allows vendors to specify their own media types for transmission online(formerly known as MIME types)
+app.use(rawParser({ type: "application/vnd.custom-type" }));
+
+app.use(textParser({ type: "text/html" }));
+
+//set express server to listen on PORT reference
 app.listen(PORT, function() {
   console.log(`The Friend Finder App is now listening on PORT ${PORT}`);
 });
