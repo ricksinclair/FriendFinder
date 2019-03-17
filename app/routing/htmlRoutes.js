78960c1f257289c79154e25bb9b45e03cd300a15
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-03-16 22:24:25 -0400

    heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,7 +8,7 @@ var app = express();
 module.exports = function(app) {
   //HTML Routes//
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/survey", function(req, res) {
